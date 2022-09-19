ff6320b1657ed08792fa5069a41961454a938d20
vikas_kumar <vikk@saxobank.com>
2022-09-19 14:36:27 +0200

    second update

--- vikas.tf
+++ vikas.tf
@@ -1,4 +1,10 @@
 resource "local_file" "vikas" {
     filename = "C:/Users/vikkadmin/Desktop/t_output/vikas.txt"
-    content = "vikas kumar singh"
+    content = "vikas kumar singh hello"
+}
+
+
+resource "local_file" "vikas" {
+    filename = "C:/Users/vikkadmin/Desktop/t_output/vicky.txt"
+    content = "vikas kumar singh bye"
 }
